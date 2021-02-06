23dfac31f1c8ad21edc0caf2ec312dab36743b15
skendanavian <s.nissen.bass@gmail.com>
2021-02-06 17:25:42 -0600

    login updates

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,29 +4,14 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import axios from "axios";
 import "./Login-Register.scss";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        The Watch List - Soren Nissen -
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -60,18 +45,15 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
 
-  const [registrationForm, setRegistrationForm] = useState({
+  const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
   });
 
-  const submitRegistration = (e) => {
+  const submitLogin = (e) => {
     e.preventDefault();
     axios
-      .post(
-        `${process.env.REACT_APP_API_URL}/register`,
-        JSON.parse(registrationForm)
-      )
+      .post(`${process.env.REACT_APP_API_URL}/login`, JSON.parse(loginForm))
       .then((res) => {
         console.log(res);
         console.log(res.data);
@@ -80,10 +62,11 @@ export default function Login() {
   };
 
   const handleInput = (e) => {
-    setRegistrationForm({
-      ...registrationForm,
+    setLoginForm({
+      ...loginForm,
       [e.target.name]: e.target.value,
     });
+    console.log(loginForm);
   };
 
   return (
@@ -96,7 +79,7 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form onSubmit={submitRegistration} className={classes.form} noValidate>
+        <form onSubmit={submitLogin} className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,10 +4,7 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -15,19 +12,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
 import "./Login-Register.scss";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
